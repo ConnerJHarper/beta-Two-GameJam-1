b6ce969a0268b38aeb29b3aefad62e9044ac97ad
firemac02 <71515767+firemac02@users.noreply.github.com>
2025-10-02 12:17:15 +0100

    Added Sound Manager

---
+++ Assets/Scripts/PlayExampleSound.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayExampleSound : MonoBehaviour
+{
+    public void PlaySound()
+    {
+        SoundManager.PlaySound(SoundType.Sound1);
+    }
+}
